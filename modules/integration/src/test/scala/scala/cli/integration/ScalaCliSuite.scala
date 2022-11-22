8ff55e94a38f926f2dfcec64a045e84b76f990c2
Łukasz Wroński <lukaszwronski98@gmail.com>
2022-11-22 16:33:18 +0100

    Measure execution of test

--- modules/integration/src/test/scala/scala/cli/integration/ScalaCliSuite.scala
+++ modules/integration/src/test/scala/scala/cli/integration/ScalaCliSuite.scala
@@ -7,6 +7,17 @@ abstract class ScalaCliSuite extends munit.FunSuite {
     Option(System.getenv("SCALA_CLI_IT_GROUP"))
       .flatMap(_.toIntOption)
       .exists(_ != group.idx)
+
+  var t0 = System.currentTimeMillis()
+
+  override def beforeAll(): Unit = {
+    t0 = System.currentTimeMillis()
+  }
+
+  override def afterAll(): Unit = {
+    val t1 = System.currentTimeMillis()
+    System.err.println(s"Elapsed time: ${this.getClass.getName} " + ((t1 - t0)/1000) + "s")
+  }
 }
 
 object ScalaCliSuite {
